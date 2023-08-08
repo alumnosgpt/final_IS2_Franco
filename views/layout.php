37068dc45e8daf143c6b5692e03bd6da6bb5e51e
lestermaudiel <flestermaudiel2130@gmail.com>
2023-08-08 02:00:39 -0600

    se actualizo boton de menu.

--- views/layout.php
+++ views/layout.php
@@ -52,7 +52,7 @@
                 </ul> 
                 <div class="col-lg-1 d-grid mb-lg-0 mb-2">
                     <!-- Ruta relativa desde el archivo donde se incluye menu.php -->
-                    <a href="/menu/" class="btn btn-danger"><i class="bi bi-arrow-bar-left"></i>MENÚ</a>
+                    <a href="/final_IS2_Franco/" class="btn btn-danger"><i class="bi bi-arrow-bar-left"></i>MENÚ</a>
                 </div>
             </div>
         </div>
