20531991a54e0ae2ffdbd6beeb6bd580eadd21e9
lestermaudiel <flestermaudiel2130@gmail.com>
2023-08-07 19:48:54 -0600

    se modifico index para que pueda funcionar con funciones

--- src/js/especialidades/index.js
+++ src/js/especialidades/index.js
@@ -1,8 +1,7 @@
-import { Dropdown } from "bootstrap";
 import Swal from "sweetalert2";
-import { validarFormulario, Toast, confirmacion} from "../funciones";
+import { validarFormulario, Toast, confirmacion } from "../funciones";
 
-const formulario = document.querySelector('form')
+const formulario = document.querySelector('form');
 const tablaEspecialidades = document.getElementById('tablaEspecialidades');
 const btnBuscar = document.getElementById('btnBuscar');
 const btnModificar = document.getElementById('btnModificar');
@@ -10,50 +9,48 @@ const btnGuardar = document.getElementById('btnGuardar');
 const btnCancelar = document.getElementById('btnCancelar');
 const divTabla = document.getElementById('divTabla');
 
-btnModificar.disabled = true
-btnModificar.parentElement.style.display = 'none'
-btnCancelar.disabled = true
-btnCancelar.parentElement.style.display = 'none'
+btnModificar.disabled = true;
+btnModificar.parentElement.style.display = 'none';
+btnCancelar.disabled = true;
+btnCancelar.parentElement.style.display = 'none';
 
 const guardar = async (evento) => {
     evento.preventDefault();
-    if(!validarFormulario(formulario, ['espec_id'])){
+    if (!validarFormulario(formulario, ['espec_id'])) {
         Toast.fire({
             icon: 'info',
             text: 'Debe llenar todos los datos'
-        })
-        return 
+        });
+        return;
     }
 
-    const body = new FormData(formulario)
-    body.delete('espec_id')
+    const body = new FormData(formulario);
+    body.delete('espec_id');
     const url = '/final_IS2_Franco/API/especialidades/guardar';
     const config = {
-        method : 'POST',
+        method: 'POST',
         body
-    }
+    };
 
     try {
-        const respuesta = await fetch(url, config)
+        const respuesta = await fetch(url, config);
         const data = await respuesta.json();
 
         console.log(data);
-        // return
-        
-        const {codigo, mensaje,detalle} = data;
-        let icon = 'info'
+        const { codigo, mensaje, detalle } = data;
+        let icon = 'info';
         switch (codigo) {
             case 1:
                 formulario.reset();
-                icon = 'success'
+                icon = 'success';
                 buscar();
                 break;
-        
+
             case 0:
-                icon = 'error'
-                console.log(detalle)
+                icon = 'error';
+                console.log(detalle);
                 break;
-        
+
             default:
                 break;
         }
@@ -61,144 +58,141 @@ const guardar = async (evento) => {
         Toast.fire({
             icon,
             text: mensaje
-        })
+        });
 
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 const buscar = async () => {
-
     let espec_nombre = formulario.espec_nombre.value;
     const url = `/final_IS2_Franco/API/especialidades/buscar?espec_nombre=${espec_nombre}`;
     const config = {
-        method : 'GET'
-    }
+        method: 'GET'
+    };
 
     try {
-        const respuesta = await fetch(url, config)
+        const respuesta = await fetch(url, config);
         const data = await respuesta.json();
-        
-        tablaEspecialidades.tBodies[0].innerHTML = ''
+
+        tablaEspecialidades.tBodies[0].innerHTML = '';
         const fragment = document.createDocumentFragment();
-        console.log(data);
-        // return;
-        if(data.length > 0){
+        
+        if (data.length > 0) {
             let contador = 1;
-            data.forEach( especialidad => {
+            data.forEach(especialidad => {
                 // CREAMOS ELEMENTOS
                 const tr = document.createElement('tr');
-                const td1 = document.createElement('td')
-                const td2 = document.createElement('td')
-                const td4 = document.createElement('td')
-                const td5 = document.createElement('td')
-                const buttonModificar = document.createElement('button')
-                const buttonEliminar = document.createElement('button')
+                const td1 = document.createElement('td');
+                const td2 = document.createElement('td');
+                const td3 = document.createElement('td'); // Agregado según el código que proporcionaste
+                const td4 = document.createElement('td');
+                const td5 = document.createElement('td');
+                const buttonModificar = document.createElement('button');
+                const buttonEliminar = document.createElement('button');
 
                 // CARACTERISTICAS A LOS ELEMENTOS
-                buttonModificar.classList.add('btn', 'btn-warning')
-                buttonEliminar.classList.add('btn', 'btn-danger')
-                buttonModificar.textContent = 'Modificar'
-                buttonEliminar.textContent = 'Eliminar'
+                buttonModificar.classList.add('btn', 'btn-warning');
+                buttonEliminar.classList.add('btn', 'btn-danger');
+                buttonModificar.textContent = 'Modificar';
+                buttonEliminar.textContent = 'Eliminar';
 
-                buttonModificar.addEventListener('click', () => colocarDatos(especialidad))
-                buttonEliminar.addEventListener('click', () => eliminar(especialidad.espec_id))
+                buttonModificar.addEventListener('click', () => colocarDatos(especialidad));
+                buttonEliminar.addEventListener('click', () => eliminar(especialidad.espec_id));
 
                 td1.innerText = contador;
-                td2.innerText = especialidad.espec_nombre
-                
+                td2.innerText = especialidad.espec_nombre;
                 
                 // ESTRUCTURANDO DOM
-                td4.appendChild(buttonModificar)
-                td5.appendChild(buttonEliminar)
-                tr.appendChild(td1)
-                tr.appendChild(td2)
-                tr.appendChild(td3)
-                tr.appendChild(td4)
-                tr.appendChild(td5)
+                td4.appendChild(buttonModificar);
+                td5.appendChild(buttonEliminar);
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                tr.appendChild(td3); // Agregado según el código que proporcionaste
+                tr.appendChild(td4);
+                tr.appendChild(td5);
 
                 fragment.appendChild(tr);
 
                 contador++;
-            })
-        }else{
+            });
+        } else {
             const tr = document.createElement('tr');
-            const td = document.createElement('td')
-            td.innerText = 'No existen registros'
-            td.colSpan = 5
-            tr.appendChild(td)
+            const td = document.createElement('td');
+            td.innerText = 'No existen registros';
+            td.colSpan = 5;
+            tr.appendChild(td);
             fragment.appendChild(tr);
         }
 
-        tablaEspecialidades.tBodies[0].appendChild(fragment)
+        tablaEspecialidades.tBodies[0].appendChild(fragment);
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 const colocarDatos = (datos) => {
-    formulario.espec_nombre.value = datos.espec_nombre
-    formulario.espec_id.value = datos.espec_id
+    formulario.espec_nombre.value = datos.espec_nombre;
+    formulario.espec_id.value = datos.espec_id;
 
-    btnGuardar.disabled = true
-    btnGuardar.parentElement.style.display = 'none'
-    btnBuscar.disabled = true
-    btnBuscar.parentElement.style.display = 'none'
-    btnModificar.disabled = false
-    btnModificar.parentElement.style.display = ''
-    btnCancelar.disabled = false
-    btnCancelar.parentElement.style.display = ''
-    divTabla.style.display = 'none'
+    btnGuardar.disabled = true;
+    btnGuardar.parentElement.style.display = '';
+    btnBuscar.disabled = true;
+    btnBuscar.parentElement.style.display = 'none';
+    btnModificar.disabled = false;
+    btnModificar.parentElement.style.display = '';
+    btnCancelar.disabled = false;
+    btnCancelar.parentElement.style.display = '';
+    divTabla.style.display = 'none';
 
     // modalEjemploBS.show();
-}
+};
 
 const cancelarAccion = () => {
-    btnGuardar.disabled = false
-    btnGuardar.parentElement.style.display = ''
-    btnBuscar.disabled = false
-    btnBuscar.parentElement.style.display = ''
-    btnModificar.disabled = true
-    btnModificar.parentElement.style.display = 'none'
-    btnCancelar.disabled = true
-    btnCancelar.parentElement.style.display = 'none'
-    divTabla.style.display = ''
-}
+    btnGuardar.disabled = false;
+    btnGuardar.parentElement.style.display = '';
+    btnBuscar.disabled = false;
+    btnBuscar.parentElement.style.display = '';
+    btnModificar.disabled = true;
+    btnModificar.parentElement.style.display = 'none';
+    btnCancelar.disabled = true;
+    btnCancelar.parentElement.style.display = 'none';
+    divTabla.style.display = '';
+};
 
 const modificar = async () => {
-    if(!validarFormulario(formulario)){
+    if (!validarFormulario(formulario)) {
         alert('Debe llenar todos los campos');
-        return 
+        return;
     }
 
-    const body = new FormData(formulario)
+    const body = new FormData(formulario);
     const url = '/final_IS2_Franco/API/especialidades/modificar';
     const config = {
-        method : 'POST',
+        method: 'POST',
         body
-    }
+    };
 
     try {
-        // fetch(url, config).then( (respuesta) => respuesta.json() ).then(d => data = d)
-        const respuesta = await fetch(url, config)
+        const respuesta = await fetch(url, config);
         const data = await respuesta.json();
-        
-        const {codigo, mensaje,detalle} = data;
-        let icon = 'info'
+
+        const { codigo, mensaje, detalle } = data;
+        let icon = 'info';
         switch (codigo) {
             case 1:
                 formulario.reset();
-                icon = 'success'
+                icon = 'success';
                 buscar();
                 cancelarAccion();
                 break;
-        
+
             case 0:
-                icon = 'error'
-                console.log(detalle)
+                icon = 'error';
+                console.log(detalle);
                 break;
-        
+
             default:
                 break;
         }
@@ -206,56 +200,57 @@ const modificar = async () => {
         Toast.fire({
             icon,
             text: mensaje
-        })
+        });
 
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 const eliminar = async (id) => {
-    if(await confirmacion('warning','¿Desea eliminar este registro?')){
-        const body = new FormData()
-        body.append('espec_id', id)
+    if (await confirmacion('warning', '¿Desea eliminar este registro?')) {
+        const body = new FormData();
+        body.append('espec_id', id);
         const url = '/final_IS2_Franco/API/especialidades/eliminar';
         const config = {
-            method : 'POST',
+            method: 'POST',
             body
-        }
+        };
         try {
-            const respuesta = await fetch(url, config)
+            const respuesta = await fetch(url, config);
             const data = await respuesta.json();
-            console.log(data)
-            const {codigo, mensaje,detalle} = data;
-    
-            let icon = 'info'
+            console.log(data);
+            const { codigo, mensaje, detalle } = data;
+
+            let icon = 'info';
             switch (codigo) {
                 case 1:
-                    icon = 'success'
+                    icon = 'success';
                     buscar();
                     break;
-            
+
                 case 0:
-                    icon = 'error'
-                    console.log(detalle)
+                    icon = 'error';
+                    console.log(detalle);
                     break;
-            
+
                 default:
                     break;
             }
-    
+
             Toast.fire({
                 icon,
                 text: mensaje
-            })
-    
+            });
+
         } catch (error) {
             console.log(error);
         }
     }
-}
+};
+
 buscar();
-formulario.addEventListener('submit', guardar )
-btnBuscar.addEventListener('click', buscar)
-btnCancelar.addEventListener('click', cancelarAccion)
-btnModificar.addEventListener('click', modificar)
+formulario.addEventListener('submit', guardar );
+btnBuscar.addEventListener('click', buscar);
+btnCancelar.addEventListener('click', cancelarAccion);
+btnModificar.addEventListener('click', modificar);
