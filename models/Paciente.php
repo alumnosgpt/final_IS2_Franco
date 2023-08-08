8a2409b4f662b734cf1d1781deeb92473b281c99
lestermaudiel <flestermaudiel2130@gmail.com>
2023-08-07 23:35:26 -0600

    creacion de paciente para modelos

---
+++ models/Paciente.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Model;
+
+class Paciente extends ActiveRecord{
+    public static $tabla = 'pacientes';
+    public static $columnasDB = ['paciente_nombre','paciente_dpi','paciente_telefono','paciente_situacion'];
+    public static $idTabla = 'espec_id';
+
+    public $espec_id;
+    public $paciente_nombre;
+    public $paciente_dpi;
+    public $paciente_telefono;
+    public $paciente_situacion;
+
+    public function __construct($args =[])
+    {
+        $this->espec_id = $args['espec_id'] ?? null;
+        $this->paciente_nombre = $args['paciente_nombre'] ?? '';
+        $this->paciente_dpi = $args['paciente_dpi'] ?? '';
+        $this->paciente_telefono = $args['paciente_telefono'] ?? '';
+        $this->paciente_situacion = $args['paciente_situacion'] ?? '1';
+    }
+
+}
